ee67bb662e67c3dd32c810dfa5f65d7c107dbd92
Alisher-Umarov <umarovalisher132@gmail.com>
2023-02-06 20:24:07 +0500

    fixed

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -41,7 +41,7 @@ const Footer: FC = () => {
                     </Link>
                 </div>
                 <div className='flex justify-center mt-5'>
-                    <p className='text-slate-300 text-1xl'>Copyright © 2023 • Alisher's blog - Alisher's coding journey</p>
+                    <p className='text-slate-300 text-1xl'>Copyright © 2023 • Alisher's blog - All rights reserved.</p>
                 </div>
             </div>
         </>
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,13 +3,16 @@ import { FC } from 'react'
 const Home: FC = () => {
   return (
     <>
-    <title>Alisher Umarov</title>
-      <div style={{ width: '60%', left: '-210px' }} className='container pt-40 relative'>
+      <title>Alisher Umarov</title>
+      <div className='mw-[60%] '>
+        <div className='container pt-40'>
           <h1
             className='text-white text-5xl'
           >
             Hey, I’m Alisher, a frontend developer
           </h1>
+          <p className='text-white py-3 w-[90%]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias architecto sequi exercitationem eos, sint neque quam nostrum assumenda repellat fugiat!</p>
+        </div>
       </div>
     </>
   )
