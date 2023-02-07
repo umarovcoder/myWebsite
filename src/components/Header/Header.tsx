9cd2bc22084bdd75ad9972bcb0a85e8d5a922c05
Alisher-Umarov <umarovalisher132@gmail.com>
2023-02-07 18:18:55 +0500

    added styles to navbar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,16 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
+import './Header.scss'
 
 const Header: FC = () => {
+
+    window.addEventListener('scroll', function () {
+        const header: any = this.document.querySelector('header');
+        header.classList.toggle('sticky', window.scrollY >= 0)
+    });
+
     return (
-        <div className='container flex justify-between py-5'>
+        <header className='container flex justify-between py-5'>
             <h2 className="text-3xl font-bold cursor-pointer text-white">
                 <Link to={'/'}>Alisher <br /> Umarov.</Link>
             </h2>
@@ -14,28 +21,28 @@ const Header: FC = () => {
                 >
                     <Link to={'/'}>Home</Link>
                 </li>
-                <li 
+                <li
                     className='text-xl cursor-pointer hover:underline underline-offset-4'
                 >
                     <Link to={'/about'}>About</Link>
                 </li>
-                <li 
+                <li
                     className='text-xl cursor-pointer hover:underline underline-offset-4'
                 >
                     <Link to={'/articles'}>Articles</Link>
                 </li>
-                <li 
+                <li
                     className='text-xl cursor-pointer hover:underline underline-offset-4'
                 >
                     <Link to={'/projects'}>Projects</Link>
                 </li>
-                <li 
+                <li
                     className='text-xl cursor-pointer hover:underline underline-offset-4'
                 >
                     <Link to={'/contact'}>Contact</Link>
                 </li>
             </ul>
-        </div>
+        </header>
     )
 }
 
