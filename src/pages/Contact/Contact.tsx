5834f74180a0706e4b33fa4afb511b95c1e41137
Alisher-Umarov <umarovalisher132@gmail.com>
2023-02-25 13:11:33 +0500

    fixed

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -4,7 +4,16 @@ const Contact: FC = () => {
   return (
     <>
     <title>Contact - Alisher</title>
-      <div className='container text-white'>Contact</div>
+      <div className='container text-white flex justify-center'>
+        <form>
+          <div className='flex flex-col gap-4 w-[100%] pt-28'>
+            <h1>Contact with me</h1>
+            <input className='w-[500px] p-2 rounded-sm outline-none text-black' type="text" placeholder='Name' />
+            <input className='w-[500px] p-2 rounded-sm outline-none text-black' type="email" placeholder='Email' />
+            <input className='w-[500px] p-2 rounded-sm outline-none text-black' type="number" placeholder='Number' />
+          </div>
+        </form>
+      </div>
     </>
   )
 }
