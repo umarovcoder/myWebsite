150c2df382813a3578f3016b66d7d2e354bbd7c5
Alisher Umarov <umarovalisher132@gmail.com>
2024-07-30 11:27:50 +0500

    ishladi

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -9,7 +9,7 @@ const About: FC = () => {
       <div className='container flex justify-between pt-32 gap-[10rem]'>
         <div className='flex-col w-2/4'>
           <h1 className='text-white text-5xl'>
-          Hi, I'm a Alisher Umarov, a tech and programming enthusiast
+            I'm a Alisher Umarov, a tech and programming enthusiast
           </h1>
           <p className='text-white text-1xl py-3'>I was born in 2004 in Tashkent. It's been more than 1 year since I entered the world of programming. <br />
             Currently Programming has become an integral part of my life. <br />
