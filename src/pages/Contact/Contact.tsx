89217d0261bc8328df11e14434ae8ef51af2366c
umarovcoder <umarovalisher132@gmail.com>
2025-07-27 10:51:23 +0500

    change buttons

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import './Contact.scss'
 import CommentsComponent from '../../components/comments';
 
 const Contact: FC = () => {
@@ -8,17 +9,14 @@ const Contact: FC = () => {
       <title>Contact - Alisher</title>
       <div className='container text-white'>
         <form className='telegram-form'>
-
-          {/* input formlar */}
-
-          {/* <div className='flex flex-col gap-4 w-[100%] pt-28'>
+           <div className='flex flex-col gap-4 w-[100%] pt-28'>
             <h1>Contact with me</h1>
             <input className='w-[500px] p-2 rounded-sm outline-none text-black' type="text" placeholder='Name' name='name' />
             <input className='w-[500px] p-2 rounded-sm outline-none text-black' type="email" placeholder='Email' name='email' />
             <input className='w-[500px] p-2 rounded-sm outline-none text-black' type="number" placeholder='Number' name='phone' />
             <textarea className='w-[500px] p-2 rounded-sm outline-none text-black' placeholder='Description' name="text"></textarea>
             <button type='submit'>Send</button>
-          </div> */}
+          </div> 
           <CommentsComponent />
         </form>
       </div>
