a88915fd3ceee0a335dfe25ab7a84cc6f3b822bd
umarovcoder <umarovalisher132@gmail.com>
2025-07-27 10:58:56 +0500

    changed form component

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -9,13 +9,13 @@ const Contact: FC = () => {
       <title>Contact - Alisher</title>
       <div className='container text-white'>
         <form className='telegram-form'>
-           <div className='flex flex-col gap-4 w-[100%] pt-28'>
+           <div className='flex flex-col justify-center items-center gap-4 w-[100%] pt-28'>
             <h1>Contact with me</h1>
             <input className='w-[500px] p-2 rounded-sm outline-none text-black' type="text" placeholder='Name' name='name' />
             <input className='w-[500px] p-2 rounded-sm outline-none text-black' type="email" placeholder='Email' name='email' />
             <input className='w-[500px] p-2 rounded-sm outline-none text-black' type="number" placeholder='Number' name='phone' />
             <textarea className='w-[500px] p-2 rounded-sm outline-none text-black' placeholder='Description' name="text"></textarea>
-            <button type='submit'>Send</button>
+            <button className='button1' type='submit'>Send</button>
           </div> 
           <CommentsComponent />
         </form>
