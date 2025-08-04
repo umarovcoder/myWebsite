82260990af9b603284351ed7d5c2a24128797ff7
umarovcoder <umarovalisher132@gmail.com>
2025-08-04 18:22:56 +0500

    t

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,15 +4,15 @@ const Home: FC = () => {
   return (
     <>
       <title>Alisher Umarov</title>
-        <div className='container pt-32'>
-          <h1
-            className='text-white text-5xl w-[50%] select-none'
-          >
-            Hey, I’m Alisher, a frontend developer
-          </h1>
-          <p className='text-white text-1xl py-3 w-[60%]'>I am passionate about programming and love Linux. I am always eager to learn new technologies, write clean code, and build perfect user interfaces. For me, frontend development is not just a job but a true passion.
+      <div className='container pt-32'>
+        <h1
+          className='text-white text-5xl w-[50%] select-none'
+        >
+          Hey, I’m Alisher, a frontend developer
+        </h1>
+        <p className='text-white text-1xl py-3 w-[60%]'>I am passionate about programming and love Linux. I am always eager to learn new technologies, write clean code, and build perfect user interfaces. For me, frontend development is not just a job but a true passion.
 
-</p>
+        </p>
       </div>
     </>
   )
