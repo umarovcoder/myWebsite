43f453f3f54f12879c44ba6504ddf2463507e524
Alisher Umarov <87524321+umarovcoder@users.noreply.github.com>
2026-09-22 11:49:10 +0500

    Add Uzbek-first language provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import { Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import { routes } from './routes';
+import { LanguageProvider } from './context/LanguageContext';
 
 const App: FC = () => {
   return (
-    <div>
-      <Header />
-      <Routes>
-        {routes.map(({ path, element }) => (
-          <Route path={path} element={element} />
-        ))}
-      </Routes>
-      <Footer />
-    </div>
+    <LanguageProvider>
+      <div>
+        <Header />
+        <Routes>
+          {routes.map(({ path, element }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
+        </Routes>
+        <Footer />
+      </div>
+    </LanguageProvider>
   )
 }
 
